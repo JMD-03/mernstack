b47bdca16e38543691a1e3ca488a9b662cc49a1c
Jacob DeCamp <jacobdecamp03@gmail.com>
2019-04-15 17:01:38 -0500

    First Commit

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+// @route GET api/users/tests
+// @description Tests users route
+//@access   Public Route
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
 module.exports = router;
