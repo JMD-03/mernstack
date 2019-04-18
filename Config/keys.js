c3aa1cd29978811e80ea58a019139082b315f6ef
Jacob DeCamp <jacobdecamp03@gmail.com>
2019-04-18 17:22:39 -0500

    Commit Test after editing keys.js file

--- Config/keys.js
+++ Config/keys.js
@@ -2,14 +2,3 @@ module.exports = {
   mongoURI:
     "mongodb+srv://Jacob:Titan121@mernstackapp-3bkso.mongodb.net/test?retryWrites=true"
 };
-
-// const MongoClient = require("mongodb").MongoClient;
-
-// const uri =
-//   "mongodb+srv://Jacob:Titan23121@mernstackapp-3bkso.mongodb.net/test?retryWrites=true";
-// const client = new MongoClient(uri, { useNewUrlParser: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
