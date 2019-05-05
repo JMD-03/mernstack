7c19ee7319cc47cab83beec3488e4ec8e69208d1
Jacob DeCamp <jacobdecamp03@gmail.com>
2019-05-05 11:45:20 -0500

    NowPreparingToDeploy

--- Config/keys.js
+++ Config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    'mongodb+srv://Jacob:Titan121@mernstackapp-3bkso.mongodb.net/test?retryWrites=true',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
